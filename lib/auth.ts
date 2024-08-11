d0fde9970cd1e695c85cd1740c4a4e700e6fbd94
sthorpe3 <39708939+sthorpe3@users.noreply.github.com>
2024-08-11 18:36:29 -0400

    nextauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -10,6 +10,7 @@ export const authOptions: NextAuthOptions = {
     GitHubProvider({
       clientId: process.env.AUTH_GITHUB_ID as string,
       clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+      secret: process.env.NEXTAUTH_SECRET as string,
       profile(profile) {
         return {
           id: profile.id.toString(),
